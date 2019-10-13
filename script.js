a7cc835992042be0444ad6127df31f7906efde11
Steven <sskim14@gmail.com>
2019-10-12 21:53:21 -0400

    Completed code for the newDeck function

--- script.js
+++ script.js
@@ -15,5 +15,17 @@ class Deck {
             return this.face
             return this.value
         }
+
+        let suits = ["Spades", "Hearts", "Clubs", "Diamonds"];
+        let faces = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"];
+        let values = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14];
+        
+        for (let s = 0; s < suits.length; s++) {
+            for (let f = 0; f < faces.length; f++) {
+                for (let v = 0; v < values.length; v++) {
+                    this.deck.push(card(suits[s], faces[f], values[v]))
+                }
+            }
+        }
     }
 }
