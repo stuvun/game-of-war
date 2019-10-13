b594d03a9090afbb69bcba512dc88fec9389d86b
Steven <sskim14@gmail.com>
2019-10-12 21:51:37 -0400

    Added base function for generating a full deck

--- script.js
+++ script.js
@@ -4,4 +4,16 @@ class Deck {
         this.player1 = []
         this.player2 = []
     }
+
+    newDeck () {
+        let card = (suit, face, value) => {
+            this.name = face + " of " + suit
+            this.face = face
+            this.value = value
+
+            return this.name
+            return this.face
+            return this.value
+        }
+    }
 }
